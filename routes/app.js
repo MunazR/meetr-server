c6c76142f16321042554a64fc07649a93ff42149
Munaz Rahman <Munaz Rahman>
2016-01-16 16:48:02 -0500

    Added meeting rooms including the ability to retrieve meeting rooms and book meeting rooms

--- routes/app.js
+++ routes/app.js
@@ -3,3 +3,117 @@ exports.getHealth = function(req, res) {
     server: "OK"
   });
 };
+
+exports.getAllMeetingRooms = function(req, res) {
+  var db = req.db;
+  var collection = db.get('meetingRooms')
+  var query = {};
+
+  collection.find(query, function(err, data) {
+    if (err) {
+      return res.send(500, {
+        status: "Error",
+        message: "An error occurred: " + err.message
+      });
+    }
+
+    if (!data || data.length === 0) {
+      return res.send(200, {
+        status: "OK",
+        message: "No rooms found",
+        data: data
+      });
+    }
+
+    res.send(200, {
+      status: "OK",
+      data: data
+    });
+  });
+};
+
+exports.getAllMeetingRoomsById = function(req, res) {
+  var db = req.db;
+  var collection = db.get('meetingRooms')
+  var id = req.body.id;
+
+  collection.findById(id, function(err, data) {
+    if (err) {
+      return res.send(500, {
+        status: "Error",
+        message: "An error occurred: " + err.message
+      });
+    }
+
+    if (!data) {
+      return res.send(200, {
+        status: "OK",
+        message: "No room found with that id",
+        data: data
+      });
+    }
+
+    res.send(200, {
+      status: "OK",
+      data: data
+    });
+  });
+};
+
+exports.addMeetingRoom = function(req, res) {
+  var db = req.db;
+  var collection = db.get('meetingRooms');
+  var room = req.body;
+
+  collection.insertOne(room, function(err, data) {
+    if (err) {
+      return res.send(500, {
+        status: "Error",
+        message: "An error occurred: " + err.message
+      });
+    }
+
+    res.send(200, {
+      status: "OK",
+      message: "Added room",
+      data: data
+    });
+  });
+};
+
+exports.bookMeetingRoomById = function(req, res) {
+  var db = req.db;
+  var collection = db.get('meetingRooms');
+  var roomId = req.body.id;
+  var startTime = new Date(req.body.startTime);
+  var endTime = new Date(req.body.endTime);
+  var query = {
+    _id: roomId
+  };
+
+  var booking = {
+    startTime: startTime,
+    endTime: endTime
+  };
+
+  var update = {
+    "$push": {
+      bookings: booking
+    }
+  };
+
+  collection.update(query, update, function(err, data) {
+    if (err) {
+      return res.send(500, {
+        status: "Error",
+        message: "An error occurred: " + err.message
+      });
+    }
+
+    res.send(200, {
+      status: "OK",
+      message: "Added booking",
+      data: data
+    });
+  });
+}
--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(function(req, res, next) {
 });
 
 app.get('/health', routes.getHealth);
+app.get('/meetingRooms', routes.getAllMeetingRooms);
+app.post('/meetingRoomById', routes.getAllMeetingRoomsById);
+app.post('/bookMeetingRoomById', routes.bookMeetingRoomById);
+app.post('/addMeetingRoom', routes.addMeetingRoom);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
